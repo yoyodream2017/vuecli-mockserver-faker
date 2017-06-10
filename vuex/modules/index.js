cf88533b1f49e24049f4e7b0c01210a85a02af34
YOYO <18328347218@163.com>
2017-06-10 09:54:09 +0800

    1

--- vuex/modules/index.js
+++ vuex/modules/index.js
@@ -1,9 +1,14 @@
 const files = require.context('.', false, /\.js$/)
+// files是一个函数，可以再调用keys()
+// console.log(files)
 const modules = {}
-
+// console.log(files.keys())// 返回map["./index.js", "./products.js", "./user.js"]
+// 下面提到的key理解为value更好
 files.keys().forEach((key) => {
+//  console.log(files(key))
   if (key === './index.js') return
+  // 找到不是./index.js的部分,去掉前面的./和后面的.js作为modules的key
   modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
 })
-
+// console.log(modules) //Object {products: Object, user: Object}
 export default modules
--- vuex/store.js
+++ vuex/store.js
@@ -5,6 +5,6 @@ import modules from './modules'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules,
+  modules,// Object {products: Object, user: Object},后面每个object含有actions，getters等
   strict: process.env.NODE_ENV !== 'production'
 })
