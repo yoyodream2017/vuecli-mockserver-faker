be88fb9993f25dc9c98fa700894d9f34b5578e0e
yoyo <18328347218@163.com>
2017-08-16 20:49:06 +0800

    add scrollbar component

---
+++ src/components/VueScrollbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <vue-scrollbar classes="my-scrollbar" ref="Scrollbar">
+      <div class="scroll-me">
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="kolom"></div>
+        <div class="clearfix"></div>
+      </div>
+    </vue-scrollbar>
+  </div>
+</template>
+
+<script>
+
+  import VueScrollbar from 'vue2-scrollbar'
+  require('../assets/vue2-scrollbar.css')
+  require('../assets/app.css')
+
+  export default {
+    components: { VueScrollbar }
+  }
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-
+import Scrollbar from '../components/VueScrollbar'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/scrollbar',
+      component: Scrollbar
     }
   ]
 })
