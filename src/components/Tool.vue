923bb9dc8359bc4954a2efa2d8a2b4d05702adef
Leon Yu <lyu@Leon-Yus-MBP.local>
2017-09-25 09:22:57 +0800

    js-tool add new sort number function

--- src/components/Tool.vue
+++ src/components/Tool.vue
@@ -12,6 +12,10 @@
     <div style='display: flex;'>
       <div style='margin-right: 10px;' v-for='item in sortByName'>{{item.name}}---{{item.age}}</div>
     </div>
+    <div class='header'>SortByAge</div>
+    <div style='display: flex;'>
+      <div style='margin-right: 10px;' v-for='item in sortByAge'>{{item.name}}---{{item.age}}</div>
+    </div>
     <div class='header'>filterOnAge</div>
     <div style='display: flex;'>
       <div style='margin-right: 10px;' v-for='item in filterOnAge'>{{item.name}}---{{item.age}}</div>
@@ -26,21 +30,26 @@ export default {
       testArray: [
         { id: 1, name: 'a3', age: '18' },
         { id: 2, name: 'a2', age: '20' },
-        { id: 3, name: 'a1', age: '19' }
+        { id: 3, name: 'a1', age: '19' },
+        { id: 4, name: 'a01', age: '182' }
       ],
       highlightId: [1, 2]
     }
   },
   computed: {
     addHighlightClass () {
-      // use map and Object.assign to add a new attribute
+      // Use map and Object.assign to add a new attribute
       return this.testArray.map(item => Object.assign(item, {
         highlight: this.highlightId.includes(item.id)
       }))
     },
     sortByName () {
-      // sort would change the original testArray, use slice() to make a copy, you can test the behaviour when there is no slice function.
+      // Sort would change the original testArray, use slice() to make a copy, you can test the behaviour when there is no slice function.
       return this.testArray.slice().sort((a, b) => a.name > b.name)
+    },
+    sortByAge () {
+      // Notice: age is String type, not suitable for number-comparison. use + to change String to Number. use ''+ to change Number to String
+      return this.testArray.slice().sort((a, b) => +a.age > +b.age)
     },
     filterOnAge () {
       return this.testArray.filter(item => item.age > 19)
